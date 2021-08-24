b2be67e5d09114b838964c2b6757814a551a473f
Nasir <nasirlawal001@gmail.com>
2021-08-25 00:19:58 +0100

    design page footer

--- src/layouts/Index.vue
+++ src/layouts/Index.vue
@@ -1,18 +1,21 @@
 <template>
   <top-bar />
   <overview />
+  <app-footer />
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 import TopBar from '../components/TopBar.vue';
 import Overview from '../components/Overview';
+import AppFooter from '../components/AppFooter';
 
 export default defineComponent({
   name: 'Index',
   components: {
     TopBar,
-    Overview
+    Overview,
+    AppFooter
   }
 })
 </script>
