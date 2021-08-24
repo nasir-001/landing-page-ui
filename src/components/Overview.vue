cef0d6e732ca581ef216839ff1ece9b36537b7da
Nasir <nasirlawal001@gmail.com>
2021-08-25 00:13:34 +0100

    add bexils aims

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -1,19 +1,22 @@
 <template>
-  <div class="lg:tw-flex tw-justify-between md:tw-w-5/6 tw-mx-auto tw-mt-10 md:tw-mt-20">
-    <div class="tw-uppercase tw-mx-2 md:tw-mx-0 lg:tw-w-4/6 tw-text-white md:tw-tracking-wide tw-font-serif md:tw-text-center lg:tw-text-left tw-text-2xl sm:tw-text-3xl md:tw-text-5xl">
-      bexils technology
-      <br />
-      <div class="tw-font-thin tw-mt-2 tw-tracking-tighter tw-text-3xl sm:tw-text-4xl md:tw-text-5xl tw-font-sans tw-text-green-400">
-        & computer solutions
-      </div> 
-      <p class="md:tw-text-lg tw-mr-2 tw-text-sm tw-text-white tw-tracking-normal tw-font-thin tw-ml-1 tw-mt-8 tw-font-mono">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis fuga expedita commodi veritatis dignissimos fugit magnam, reiciendis, hic quae suscipit voluptas velit consequuntur culpa voluptatem maxime aspernatur. Necessitatibus, nam reprehenderit.
-      </p>
+  <div class="tw-justify-between md:tw-w-5/6 tw-mx-auto tw-mt-10 md:tw-mt-20">
+      <div class="tw-uppercase tw-mx-2 md:tw-mx-0 lg:tw-w-3/6 tw-text-white md:tw-tracking-wide tw-font-serif md:tw-text-center lg:tw-text-left tw-text-2xl sm:tw-text-3xl md:tw-text-5xl">
+        bexil's computer
+        <br />
+        <div class="tw-font-thin tw-mt-2 tw-text-3xl tw-tracking-wider sm:tw-text-4xl md:tw-text-5xl tw-font-sans tw-text-green-400">
+          & solutions
+        </div> 
+        <p class="md:tw-text-lg tw-mr-2 tw-text-sm tw-text-white tw-tracking-normal tw-font-thin tw-ml-1 tw-mt-8 tw-font-mono">
+          BEXIL  COMPUTER  &  SOLUTION  LIMITED  provides  one  stop solution for your general ICT (Information and Communication Technology) services. We have different products and services to meet your requirements, depending on the size and field of your organization. We provide the optimum and customized solutions made for your organization. 
+        </p>
+      </div>
+    <div class="tw-mx-auto tw-italic tw-text-center tw-mt-10 tw-text-white tw-text-4xl">
+      <span class="">tested</span> <span class="tw-not-italic tw-text-green-400 tw-font-thin">and</span> <span>trusted</span> <br /> services
     </div>
-    <div class="lg:tw-w-1/2 tw-mx-1 md:tw-mx-0 tw-mt-6 md:tw-mt-0">
-      
+    <div class="tw-flex tw-justify-evenly tw-mx-auto">
+
     </div>
-  </div>
+    </div>
 </template>
 
 <script>
--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,17 +1,15 @@
 <template>
-  <div class="tw-flex tw-justify-between tw-mx-2 md:tw-mx-28 tw-mt-16">
+  <div class="tw-flex tw-justify-between tw-mx-5 sm:tw-mx-10 md:tw-mx-28 tw-mt-5 sm:tw-mt-12 md:tw-mt-16">
     <div class="flex tw-justify-start tw--mt-4">
-      <div class="tw-mt-5 sm:tw-mt-0">
-        <!-- <img class="tw-w-10 tw-h-10 sm:tw-w-14 sm:tw-h-14 tw-object-cover"  src="/img/logo.jpg" alt=""> -->
-      </div>
-      <div class="tw-text-xl tw-mt-6 sm:tw-mt-2 sm:tw-text-2xl tw-cursor-pointer tw-uppercase tw-text-green-400 hover:tw-text-green-500">
+      <div class="tw-text-xl tw-mt-6 sm:tw-mt-2 sm:tw-text-2xl tw-cursor-pointer tw-uppercase tw-text-white hover:tw-text-white">
         <router-link to="/">
-          bexils tech
+          bexils
         </router-link>
       </div>
     </div>
-    <div class="md:tw-flex tw-hidden tw-justify-evenly tw--mt-2">
-      
+    <div class="tw-flex tw-justify-start md:tw--mt-6">
+      <div class="tw-bg-green-400 tw-rounded md:tw-h-12 tw-h-10 tw-w-10 md:tw-w-12"></div>
+      <div class="tw-bg-green-400 tw-rounded md:tw-h-12 tw-h-10 tw-w-10 md:tw-w-12 tw-mt-3 md:tw-mt-4 tw--ml-4 md:tw--ml-5"></div>
     </div>
   </div>
 </template>
