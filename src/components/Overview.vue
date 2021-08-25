be5545f30c8475882820019438ad571c5684a9ec
Nasir <nasirlawal001@gmail.com>
2021-08-25 09:00:42 +0100

    add get in touch button

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -10,6 +10,9 @@
           BEXIL  COMPUTER  &  SOLUTION  LIMITED  provides  one  stop solution for your general ICT (Information and Communication Technology) services. We have different products and services to meet your requirements, depending on the size and field of your organization. We provide the optimum and customized solutions made for your organization. 
         </p>
       </div>
+      <div class="tw-py-2 tw-px-3 tw-bg-green-500 hover:tw-bg-green-400 hover:tw-text-gray-200 tw-text-white tw-text-lg tw-rounded-full tw-w-44 tw-text-center tw-mt-6">
+        <q-btn dense flat rounded icon="email"></q-btn><a class="tw-ml-2 tw-text-xs lg:tw-text-base" href="mailto:bexilcomputer@gmail.com">Get in touch</a>
+      </div>
     <div class="tw-mx-auto tw-italic tw-text-center tw-mt-10 tw-text-white tw-text-4xl">
       <span class="">tested</span> <span class="tw-not-italic tw-text-green-400 tw-font-thin">and</span> <span>trusted</span> <br /> services
     </div>
