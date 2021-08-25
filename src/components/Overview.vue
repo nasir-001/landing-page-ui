8ea56fb5d0ebdfbff1fe5fef0260664774310c04
Nasir <nasirlawal001@gmail.com>
2021-08-25 15:01:56 +0100

    style(services): edit company services flex

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -11,9 +11,9 @@
           BEXIL  COMPUTER  &  SOLUTION  LIMITED  provides  one  stop solution for your general ICT (Information and Communication Technology) services. We have different products and services to meet your requirements, depending on the size and field of your organization. We provide the optimum and customized solutions made for your organization. 
         </p>
       </div>
-      <div class="tw-mx-auto">
+      <div class="sm:tw-mx-auto tw-mx-4">
         <div class="tw-mt-4">
-          <img class="tw-object-cover tw-shadow-xl tw-w-44 tw-mx-auto hover:tw-shadow-2xl tw-rounded-3xl tw-overflow-hidden" src="/img/pexels-shvets-production-7194915.jpg" alt="">
+          <img class="tw-object-cover tw-shadow-xl tw-w-full sm:tw-w-44 tw-mx-auto hover:tw-shadow-2xl tw-rounded-3xl tw-overflow-hidden" src="/img/pexels-shvets-production-7194915.jpg" alt="">
           <div class="tw-text-center tw-text-white tw-font-serif tw-text-lg">
             C.E.O
           </div>
@@ -23,7 +23,7 @@
         </div>
         <div class="sm:tw-flex sm:tw-flex-wrap tw-justify-start tw-mt-4">
           <div class="sm:tw-mr-10">
-            <img class="tw-object-cover tw-shadow-xl tw-w-44 tw-mx-auto hover:tw-shadow-2xl tw-rounded-3xl tw-overflow-hidden" src="/img/pexels-shvets-production-7194915.jpg" alt="">
+            <img class="tw-object-cover tw-shadow-xl tw-w-full sm:tw-w-44 tw-mx-auto hover:tw-shadow-2xl tw-rounded-3xl tw-overflow-hidden" src="/img/pexels-shvets-production-7194915.jpg" alt="">
             <div class="tw-text-center tw-text-white tw-font-serif tw-text-lg">
               C.T.O
             </div>
@@ -32,7 +32,7 @@
             </div>
           </div>
           <div class="sm:tw-ml-10">
-            <img class="tw-object-cover tw-shadow-xl tw-w-44 tw-mx-auto hover:tw-shadow-2xl tw-rounded-3xl tw-overflow-hidden" src="/img/pexels-shvets-production-7194915.jpg" alt="">
+            <img class="tw-object-cover tw-shadow-xl tw-w-full sm:tw-w-44 tw-mx-auto hover:tw-shadow-2xl tw-rounded-3xl tw-overflow-hidden" src="/img/pexels-shvets-production-7194915.jpg" alt="">
             <div class="tw-text-center tw-text-white tw-font-serif tw-text-lg">
               ***
             </div>
@@ -44,14 +44,65 @@
       </div>
     </div>
   </div>
-  <div class="sm:tw-py-2 sm:tw-px-3 tw-py-1 tw-px-2 tw-mx-2 sm:tw-mx-0 tw-bg-green-500 hover:tw-bg-green-400 hover:tw-text-gray-200 tw-text-white tw-text-lg tw-rounded-full tw-w-44 tw-text-center tw-mt-3 sm:tw-mt-6">
-    <q-btn dense flat rounded icon="email"></q-btn><a class="tw-ml-2 tw-text-xs lg:tw-text-base" href="mailto:bexilcomputer@gmail.com">Get in touch</a>
+  <div class="tw-mx-6">
+    <div class="sm:tw-py-2 sm:tw-px-3 tw-py-1 tw-px-2 sm:tw-mx-28 tw-bg-green-500 hover:tw-bg-green-400 hover:tw-text-gray-200 tw-text-white tw-text-lg tw-rounded-full tw-w-full sm:tw-w-44 tw-text-center tw-mt-6">
+      <q-btn dense flat rounded icon="email"></q-btn><a class="tw-ml-2 tw-text-xs lg:tw-text-base" href="mailto:bexilcomputer@gmail.com">Get in touch</a>
+    </div>
   </div>
   <div class="tw-mx-auto tw-italic tw-text-center tw-mt-10 tw-text-white tw-text-4xl">
     <span class="">tested</span> <span class="tw-not-italic tw-text-green-400 tw-font-thin">and</span> <span>trusted</span> <br /> services
   </div>
-  <div class="tw-flex tw-justify-evenly tw-mx-auto">
-    hllkj
+  <div class="tw-flex tw-flex-wrap tw-mx-4 sm:tw-mx-20 tw-justify-start tw-mt-8">
+    <div class="sm:tw-w-1/2 tw-w-full md:tw-w-1/3 tw-rounded-full tw-h-32">
+      <div class="tw-text-center tw-text-green-400 tw-text-xl tw-font-semibold tw-font-mono">
+        Software Development
+      </div>
+      <div class="tw-text-sm tw-text-white tw-font-mono">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est enim error voluptas tempore! Natus eaque repudiandae cupiditate ut dignissimos animi dolorem excepturi magni soluta 
+      </div>
+    </div>
+    <div class="sm:tw-w-1/2 tw-w-full md:tw-w-1/3 tw-rounded-full tw-h-32">
+      <div class="tw-text-center tw-text-green-400 tw-text-xl tw-font-semibold tw-font-mono tw-mt-8 sm:tw-mt-0">
+        Facial Recognition Integration
+      </div>
+      <div class="tw-text-sm tw-text-white tw-font-mono">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est enim error voluptas tempore! Natus eaque repudiandae cupiditate ut dignissimos animi dolorem excepturi magni soluta 
+      </div>
+    </div>
+    <div class="sm:tw-w-1/2 tw-w-full md:tw-w-1/3 tw-rounded-full tw-h-32">
+      <div class="tw-text-center tw-text-green-400 tw-text-xl tw-font-semibold tw-font-mono tw-mt-16 sm:tw-mt-0">
+        Secure-NG
+      </div>
+      <div class="tw-text-sm tw-text-white tw-font-mono">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est enim error voluptas tempore! Natus eaque repudiandae cupiditate ut dignissimos animi dolorem excepturi magni soluta 
+      </div>
+    </div>
+  </div>
+  <div class="tw-flex tw-flex-wrap tw-mx-4 sm:tw-mx-20 tw-justify-start tw-mt-8 sm:tw-mt-0">
+    <div class="sm:tw-w-1/2 tw-w-full md:tw-w-1/3 tw-rounded-full tw-h-32 tw-mt-8">
+      <div class="tw-text-center tw-text-green-400 tw-text-xl tw-font-semibold tw-font-mono tw-mt-4">
+        Transport Manifest
+      </div>
+      <div class="tw-text-sm tw-text-white tw-font-mono">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est enim error voluptas tempore! Natus eaque repudiandae cupiditate ut dignissimos animi dolorem excepturi magni soluta 
+      </div>
+    </div>
+    <div class="sm:tw-w-1/2 tw-w-full md:tw-w-1/3 tw-rounded-full tw-h-32 tw-mt-8">
+      <div class="tw-text-center tw-text-green-400 tw-text-xl tw-font-semibold tw-font-mono tw-mt-4">
+        Data Optimisation
+      </div>
+      <div class="tw-text-sm tw-text-white tw-font-mono">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est enim error voluptas tempore! Natus eaque repudiandae cupiditate ut dignissimos animi dolorem excepturi magni soluta 
+      </div>
+    </div>
+    <div class="sm:tw-w-1/2 tw-w-full md:tw-w-1/3 tw-rounded-full tw-h-32 tw-mt-8">
+      <div class="tw-text-center tw-text-green-400 tw-text-xl tw-font-semibold tw-font-mono tw-mt-4">
+        CCTV Installation and Management
+      </div>
+      <div class="tw-text-sm tw-text-white tw-font-mono">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est enim error voluptas tempore! Natus eaque repudiandae cupiditate ut dignissimos animi dolorem excepturi magni soluta 
+      </div>
+    </div>
   </div>
 </template>
 
