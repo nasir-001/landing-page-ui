47909164ca6359f167319d59fbcb1b0bef8ed564
Nasir <nasirlawal001@gmail.com>
2021-08-25 11:04:08 +0100

    add people image

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -1,25 +1,58 @@
 <template>
-  <div class="tw-justify-between md:tw-w-5/6 tw-mx-auto tw-mt-10 md:tw-mt-20">
+  <div class="md:tw-w-5/6 tw-mx-auto tw-mt-10 md:tw-mt-20">
+    <div class="tw-flex tw-flex-wrap tw-justify-between">
       <div class="tw-uppercase tw-mx-2 md:tw-mx-0 lg:tw-w-3/6 tw-text-white md:tw-tracking-wide tw-font-serif md:tw-text-center lg:tw-text-left tw-text-2xl sm:tw-text-3xl md:tw-text-5xl">
         bexil's computer
         <br />
         <div class="tw-font-thin tw-mt-2 tw-text-3xl tw-tracking-wider sm:tw-text-4xl md:tw-text-5xl tw-font-sans tw-text-green-400">
-          & solutions
+          & solutions LTD
         </div> 
         <p class="md:tw-text-lg tw-mr-2 tw-text-sm tw-text-white tw-tracking-normal tw-font-thin tw-ml-1 tw-mt-8 tw-font-mono">
           BEXIL  COMPUTER  &  SOLUTION  LIMITED  provides  one  stop solution for your general ICT (Information and Communication Technology) services. We have different products and services to meet your requirements, depending on the size and field of your organization. We provide the optimum and customized solutions made for your organization. 
         </p>
       </div>
-      <div class="sm:tw-py-2 sm:tw-px-3 tw-py-1 tw-px-2 tw-mx-2 sm:tw-mx-0 tw-bg-green-500 hover:tw-bg-green-400 hover:tw-text-gray-200 tw-text-white tw-text-lg tw-rounded-full tw-w-44 tw-text-center tw-mt-3 sm:tw-mt-6">
-        <q-btn dense flat rounded icon="email"></q-btn><a class="tw-ml-2 tw-text-xs lg:tw-text-base" href="mailto:bexilcomputer@gmail.com">Get in touch</a>
+      <div class="tw-mx-auto">
+        <div class="tw-mt-4">
+          <img class="tw-object-cover tw-shadow-xl tw-w-44 tw-mx-auto hover:tw-shadow-2xl tw-rounded-3xl tw-overflow-hidden" src="/img/pexels-shvets-production-7194915.jpg" alt="">
+          <div class="tw-text-center tw-text-white tw-font-serif tw-text-lg">
+            C.E.O
+          </div>
+          <div class="tw-text-center tw-text-white tw-font-sans tw--mt-2 tw-text-lg">
+            Yusuf Hayatuddeen
+          </div>
+        </div>
+        <div class="sm:tw-flex sm:tw-flex-wrap tw-justify-start tw-mt-4">
+          <div class="sm:tw-mr-10">
+            <img class="tw-object-cover tw-shadow-xl tw-w-44 tw-mx-auto hover:tw-shadow-2xl tw-rounded-3xl tw-overflow-hidden" src="/img/pexels-shvets-production-7194915.jpg" alt="">
+            <div class="tw-text-center tw-text-white tw-font-serif tw-text-lg">
+              C.T.O
+            </div>
+            <div class="tw-text-center tw-text-white tw-font-sans tw--mt-2 tw-text-lg">
+              Dahir. M. Dahir
+            </div>
+          </div>
+          <div class="sm:tw-ml-10">
+            <img class="tw-object-cover tw-shadow-xl tw-w-44 tw-mx-auto hover:tw-shadow-2xl tw-rounded-3xl tw-overflow-hidden" src="/img/pexels-shvets-production-7194915.jpg" alt="">
+            <div class="tw-text-center tw-text-white tw-font-serif tw-text-lg">
+              ***
+            </div>
+            <div class="tw-text-center tw-text-white tw-font-sans tw--mt-2 tw-text-lg">
+              Ahmad Danladi
+            </div>
+          </div>
+        </div>
       </div>
-    <div class="tw-mx-auto tw-italic tw-text-center tw-mt-10 tw-text-white tw-text-4xl">
-      <span class="">tested</span> <span class="tw-not-italic tw-text-green-400 tw-font-thin">and</span> <span>trusted</span> <br /> services
     </div>
-    <div class="tw-flex tw-justify-evenly tw-mx-auto">
-
-    </div>
-    </div>
+  </div>
+  <div class="sm:tw-py-2 sm:tw-px-3 tw-py-1 tw-px-2 tw-mx-2 sm:tw-mx-0 tw-bg-green-500 hover:tw-bg-green-400 hover:tw-text-gray-200 tw-text-white tw-text-lg tw-rounded-full tw-w-44 tw-text-center tw-mt-3 sm:tw-mt-6">
+    <q-btn dense flat rounded icon="email"></q-btn><a class="tw-ml-2 tw-text-xs lg:tw-text-base" href="mailto:bexilcomputer@gmail.com">Get in touch</a>
+  </div>
+  <div class="tw-mx-auto tw-italic tw-text-center tw-mt-10 tw-text-white tw-text-4xl">
+    <span class="">tested</span> <span class="tw-not-italic tw-text-green-400 tw-font-thin">and</span> <span>trusted</span> <br /> services
+  </div>
+  <div class="tw-flex tw-justify-evenly tw-mx-auto">
+    hllkj
+  </div>
 </template>
 
 <script>
